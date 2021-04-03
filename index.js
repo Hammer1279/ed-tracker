3c803dac06b79fbc9bf1ad56a97a8ef57a20f6a0
Hammer1279 <hammertechnik@gmail.com>
2021-04-03 13:48:47 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const fs = require("fs")
+const superagent = require('superagent')
+const WebSocketClient = require('websocket').client;
+const events = require("events");
+const EventStream = new events.EventEmitter();
+
+const client = new WebSocketClient();
+
+function log(data) {
+    EventStream.emit('data',data)
+    fs.appendFileSync(__dirname+'/log.txt',data+'\r\n')
+}
+
+client.on('connectFailed', function(error) {
+    log('Connect Error: ' + error.toString());
+});
+
+client.on('connect', function(connection) {
+    global.connection=connection
+    log('Connection to Server established')
+    connection.on('error', function(error) {
+        log("Connection Error: " + error.toString());
+    });
+    connection.on('close', function() {
+        log('Connection Closed');
+    });
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log(message)
+            log("Received: '" + message.utf8Data + "'");
+        }
+    });
+});
+
+function sendData(data) {
+    if(connection){
+        if (connection.connected) {
+            connection.send(data)
+        }else{
+            log('Error: Not connected yet!')
+            return
+        }
+    }else{
+        log('Error: Not Logged in yet!')
+        return
+    }
+}
+
+class EDTracker {
+    constructor (){
+        if(!process.env.ED_API_Key){log('Missing API Key');return}
+        this.login = function (key) {client.connect('ws://drillkea.com:80/', 'ed-tracker', 'ED-Tracker-Client',{apikey:key});}
+        this.events = EventStream
+        this.send = sendData
+        this.appendStream = function (stream) {
+
+        }
+        this.api = function (data) {}
+    }
+}
+
+module.exports = EDTracker
