ddf2a410e95893fa63a96a03636ac42bf5d147ce
Hammer1279 <hammertechnik@gmail.com>
2021-04-05 01:36:25 +0200

    Updated Index, Fixed appendStream | Credits: Woof

--- index.js
+++ index.js
@@ -8,43 +8,58 @@ const client = new WebSocketClient();
 
 let EventStream;
 
-class EDTracker extends events.EventEmitter{
-    constructor (){
+class EDTracker extends events.EventEmitter {
+    constructor() {
         super()
         EventStream = this
         // if(!process.env.ED_API_Key){log('Missing API Key');return}
-        this.login = function (key) {client.connect('ws://api.drillkea.com:80/', 'ed-tracker', 'ED-Tracker-Client',{apikey:key});}
+        this.login = function (key) { client.connect('ws://api.drillkea.com:80/', 'ed-tracker', 'ED-Tracker-Client', { apikey: key }); }
         this.events = this
         this.send = sendData
         this.appendStream = function (stream) {
-            const throttle = somethrottlething
-            const ThrottledStream = new events.EventEmitter();
-            stream.pipe(throttle).pipe(ThrottledStream);
-            ThrottledStream.on('newLine',data=>{sendData(data)})
+            //Cooldown = half second
+            var cooldown = 0.5 * 1000
+            var queue = new Array
+
+            stream.on('newLine', data =>addQueue(data))
+
+            //Add data here!
+            function addQueue(data) {
+                queue.push(data)
+            };
+
+            setInterval(() => {
+                //Checks for data
+                if (queue.length > 0) {
+                    //if data send first item and remove it from queue
+                    sendData(queue[0])
+                    queue.shift()
+                }
+            }, cooldown);
         }
-        
+
     }
 }
 
 function log(data) {
-    if(EventStream){EventStream.emit('data',data)}else throw('Missing EventStream')
-    fs.appendFileSync(__dirname+'/log.txt',new Date().toLocaleString()+' | '+data+'\r\n')
+    if (EventStream) { EventStream.emit('data', data) } else throw ('Missing EventStream')
+    fs.appendFileSync(__dirname + '/log.txt', new Date().toLocaleString() + ' | ' + data + '\r\n')
 }
 
-client.on('connectFailed', function(error) {
+client.on('connectFailed', function (error) {
     log('Connect Error: ' + error.toString());
 });
 
-client.on('connect', function(connection) {
-    global.connection=connection
+client.on('connect', function (connection) {
+    global.connection = connection
     log('Connection to Server established')
-    connection.on('error', function(error) {
+    connection.on('error', function (error) {
         log("Connection Error: " + error.toString());
     });
-    connection.on('close', function() {
+    connection.on('close', function () {
         log('Connection Closed');
     });
-    connection.on('message', function(message) {
+    connection.on('message', function (message) {
         if (message.type === 'utf8') {
             console.log(message)
             log("Received: '" + message.utf8Data + "'");
@@ -53,14 +68,14 @@ client.on('connect', function(connection) {
 });
 
 function sendData(data) {
-    if(connection){
+    if (connection) {
         if (connection.connected) {
             connection.send(data)
-        }else{
+        } else {
             log('Error: Not connected yet!')
             return
         }
-    }else{
+    } else {
         log('Error: Not Logged in yet!')
         return
     }
